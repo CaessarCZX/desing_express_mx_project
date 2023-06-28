17b32b72a02f832aa256223f917c7bfbebf1be59
CesarTun <ctuncetina@gmail.com>
2023-06-28 17:32:51 -0600

    feat: add header and slider whit meet us area

---
+++ src/assets/js/_custom-slider.js
@@ -0,0 +1,70 @@
+'use strict';
+
+let slideIndex = 0;
+
+const plusSlides = (numberSlide) => {
+  showSlides(slideIndex += numberSlide);
+};
+
+const currentSlides = (numberSlide) => {
+  showSlides(slideIndex = numberSlide);
+};
+
+const showSlides = (numberSlide) => {
+  const slides = document.querySelectorAll('.mySlides');
+  const dots = document.querySelectorAll('.dot');
+
+  if (numberSlide > slides.length) {
+    slideIndex = 1;
+  }
+  if (numberSlide < 1) {
+    slideIndex = slides.length;
+  }
+
+  // Hide slides
+  slides.forEach((slide) => {
+    slide.style.display = 'none';
+  });
+
+  // Remove active class from dots
+  dots.forEach((dot) => {
+    dot.classList.remove('active');
+  });
+
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].classList.add('active');
+};
+
+// let slideAutoIndex = 0
+
+const autoSlides = () => {
+  const slides = document.querySelectorAll('.mySlides');
+  const dots = document.querySelectorAll('.dot');
+
+  // Hide slides
+  slides.forEach((slide) => {
+    slide.style.display = 'none';
+  });
+
+  // Remove active class from dots
+  dots.forEach((dot) => {
+    dot.classList.remove('active');
+  });
+
+  // Increment slide index
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].classList.add('active');
+
+  setTimeout(autoSlides, 5000);
+  // Chnage image every 2 seconds
+}
+
+
+showSlides(slideIndex);
+autoSlides();
